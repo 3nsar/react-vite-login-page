691d8a203e7919cd6fda194faf1694c99c2ff71c
3nsar <84131267+3nsar@users.noreply.github.com>
2023-01-21 20:16:07 +0100

    commit

---
+++ vite-project/src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='login-container'>
+      <p>Login</p>
+      <div className='login-input'>
+        <input type="text" />
+        <input type="text" />
+        <button>Sign Up</button>
+      </div>
+    </div>
+  )
+}
+
+export default Login
