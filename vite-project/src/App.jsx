e6f2b48543b976e47aabd3a8fca8924ecdc7c74b
3nsar <84131267+3nsar@users.noreply.github.com>
2023-01-22 14:37:07 +0100

    commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,28 @@
+import { useRef } from 'react'
 import { useState } from 'react'
 import './App.css'
-import Login from './components/Login'
+import FormInput from './components/FormInput'
+
 
 function App() {
+ // const [username, setUsername] = useState("")
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    const data = new FormData(e.target)
+    console.log(data)
+  }
 
   return (
-    <div className="App">
-      <Login/>
+    <div className="app">
+     <form onSubmit={handleSubmit}>
+       <FormInput name="username" placeholder="Username"/>
+       <FormInput name="email" placeholder="Email"/>
+       <FormInput name="fullname" placeholder="Full Name"/>
+       <FormInput name="password" placeholder="Password"/>
+       <button>Submit</button>
+     </form>
+     
     </div>
   )
 }
